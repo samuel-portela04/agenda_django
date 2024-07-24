54056fa2f2ce1af0c2f09c60619372e3f41c6d18
Samuel Portela <samucaportela1@gmail.com>
2024-07-24 10:55:06 -0300

    Atualizações do projeto: ImageFiled e configuração  MEDIA_URL e MEDIA_ROOT no settings.py; ForeignKey - Model Category com ligação com chave estrangeira

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=12)
     last_name = models.CharField(max_length=15, blank=True)
@@ -8,3 +15,9 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
