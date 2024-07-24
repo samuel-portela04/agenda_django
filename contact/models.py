e5355b48104e1f0e6cfb1c76e604f2b7ff02e3f9
Samuel Portela <samucaportela1@gmail.com>
2024-07-24 11:42:57 -0300

    Criando contatos aleatórios com Faker

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=50)
     
     def __str__(self) -> str:
@@ -18,6 +23,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
